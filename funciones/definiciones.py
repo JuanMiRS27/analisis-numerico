51ebc82045ce25a3706c273d907e541b66ccb85b
Juan Miguel Restrepo S <jmrestrepo@unihumboldt.edu.co>
2026-03-02 13:41:53 -0500

    Adicion de posibilidad de comparar funciones en ejercicios 2 y 4 (Se necesita cambiar el codigo manualmente)

--- funciones/definiciones.py
+++ funciones/definiciones.py
@@ -2,8 +2,10 @@ import numpy as np
 
 # ===== EJERCICIO 1: BISECCIÓN =====
 def funcion_biseccion(lam: float) -> float:
-    return 2.5 + 0.8 * lam**2 - 3.2 * lam + np.log(lam + 1)
-
+    return lam**3 - 6*lam**2 + 11*lam - 6.5
+# Colocar:
+# return 2.5 + 0.8 * lam**2 - 3.2 * lam + np.log(lam + 1) | para el primer metodo
+# return lam**3 - 6*lam**2 + 11*lam - 6.5 | para comparar el segundo metodo
 
 # ===== EJERCICIO 2: FALSA POSICIÓN =====
 def funcion_falsa_posicion(x: float) -> float:
@@ -21,12 +23,14 @@ def derivada_g_punto_fijo(x: float) -> float:
 
 # ===== EJERCICIO 4: NEWTON =====
 def funcion_newton(n: float) -> float:
-    return n**3 - 8*n**2 + 20*n - 16
-
+    return  n**3 - 8*n**2 + 20*n - 16
+# Ejercicio 4: n**3 - 8*n**2 + 20*n - 16
+# Ejercicio 5:  n * np.exp(-n / 2) - 0.3
 
 def derivada_newton(n: float) -> float:
     return 3*n**2 - 16*n + 20
-
+# Ejercicio 4: 3*n**2 - 16*n + 20
+# Ejercicio 5: np.exp(-n / 2) * (1 - n / 2)
 
 # ===== EJERCICIO 5: SECANTE =====
 def funcion_secante(x: float) -> float:
